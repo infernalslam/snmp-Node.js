63375dd8e82f1e4a3ae1537b651e1aaa513d872e
TAK <infernal-slam@hotmail.com>
2016-05-02 13:02:32 +0700

    add mib 2 ver

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var express = require('express')
 var app = express()
 var snmp = require('snmp-native')
-var network = require('network')
+var speedTest = require('speedtest-net')
+var net_snmp = require('net-snmp')
 // var host = '10.1.160.1' // fitmwifi
 var host = '10.41.160.1' // fitmwifi
 // 10.4.15.1
@@ -12,18 +13,19 @@ var session = new snmp.Session({ host: host, community: community })
 var session2 = new snmp.Session({ host: host, community: community })
 var session3 = new snmp.Session({ host: host, community: community })
 var session4 = new snmp.Session({ host: host, community: community })
+var session5 = new snmp.Session({ host: host, community: community })
 var oid1 = [1, 3, 6, 1, 2, 1, 1]
-var oid2 = '.1.3.6.1.2.1.4.20.1.1' // ip
 var oid3 = '.1.3.6.1.2.1.4.21.1.11' // subnet
-var oid4 = '.1.3.6.1.2.1.4.21.1.1' //iproute
+var oid4 = '.1.3.6.1.2.1.4.21.1.1' // iproute
+var oid5 = '.1.3.6.1.2.1.2.2.1.2'
 var vb = []
 var ip = []
 var subnet = []
 var iproute = []
-// console.log(Name)
+var vlan = []
+var speed = []
+
 session.getSubtree({ oid: oid1 }, function (err, varbinds) {
-  // vb = varbinds[0]
-  // console.log(varbinds)
   vb.push({
     discription: varbinds[0].value,
     uptime: varbinds[2].value,
@@ -32,17 +34,6 @@ session.getSubtree({ oid: oid1 }, function (err, varbinds) {
   // console.log(vb[0].name)
   session.close()
 })
-
-session2.getSubtree({ oid: oid2 }, function (err, varbinds) {
-  varbinds.forEach(function (data) {
-    // console.log(data.value)
-    ip.push({
-      ip: data.value
-    })
-  })
-  session2.close()
-})
-
 session3.getSubtree({ oid: oid3 }, function (err, varbinds) {
   varbinds.forEach(function (data) {
     subnet.push({
@@ -53,25 +44,117 @@ session3.getSubtree({ oid: oid3 }, function (err, varbinds) {
 })
 session4.getSubtree({ oid: oid4 }, function (err, varbinds) {
   varbinds.forEach(function (data) {
-    iproute.push({iproute:data.value})
+    iproute.push({iproute: data.value})
   })
   session4.close()
 })
-network.get_interfaces_list(function(err, list) {
-  // console.log(list) // interface
+session5.getSubtree({ oid: oid5 }, function (err, varbinds) {
+  varbinds.forEach(function (data) {
+    // console.log(data.value)
+    vlan.push({vlan: data.value})
+  })
+  session5.close()
+})
+test = speedTest({maxTime: 3000})
+test.on('data', function (data) {
+  // console.dir(data)
+  speed.push(data)
+})
+// //////////////////////////////
+// admin work
+var nameinterface = new snmp.Session({ host: host, community: community })
+var oid_nameinterface = '.1.3.6.1.2.1.2.2.1.2'
+nameinterface.getSubtree({ oid: oid_nameinterface }, function (err, varbinds) {
+  varbinds.forEach(function (data) {
+    // console.log(data.value) 
+  })
+  nameinterface.close()
+})
+var type_interface = new snmp.Session({ host: host, community: community })
+var oid_inter = '.1.3.6.1.2.1.2.2.1.3'
+type_interface.getSubtree({ oid: oid_inter }, function (err, varbinds) {
+  varbinds.forEach(function (data) {
+    // console.log(data.value)
+  })
+  type_interface.close()
+})
+
+var type_mtu = new snmp.Session({ host: host, community: community })
+var oid_mtu = '.1.3.6.1.2.1.2.2.1.4'
+type_mtu.getSubtree({ oid: oid_mtu }, function (err, varbinds) {
+  varbinds.forEach(function (data) {
+    // console.log(data.value)
+  })
+  type_mtu.close()
+})
+var type_speed = new snmp.Session({ host: host, community: community })
+var oid_speed = '.1.3.6.1.2.1.2.2.1.6'
+type_speed.getSubtree({ oid: oid_speed }, function (err, varbinds) {
+  varbinds.forEach(function (data) {
+    // console.log(data.value)
+  })
+  type_speed.close()
+})
+
+var type_status = new snmp.Session({ host: host, community: community })
+var oid_status = '.1.3.6.1.2.1.2.2.1.8'
+type_status.getSubtree({ oid: oid_status }, function (err, varbinds) {
+  varbinds.forEach(function (data) {
+    // console.log(data.value) 
+  })
+  type_status.close()
+})
+
+var type_statusTime = new snmp.Session({ host: host, community: community })
+var oid_statusTime = '.1.3.6.1.2.1.2.2.1.9'
+type_statusTime.getSubtree({ oid: oid_statusTime }, function (err, varbinds) {
+  varbinds.forEach(function (data) {
+    // console.log(data.value)  
+  })
+  type_statusTime.close()
+})
+
+var iproutetype = []
+var iproute_type = new snmp.Session({ host: host, community: community })
+var oid_iproute_type = '.1.3.6.1.2.1.4.21.1.8'
+iproute_type.getSubtree({ oid: oid_iproute_type }, function (err, varbinds) {
+  varbinds.forEach(function (data) {
+    iproutetype.push({type: data.value})
+  })
+  iproute_type.close()
+})
+
+var iprouteprotocol = []
+var iproute_protocol = new snmp.Session({ host: host, community: community })
+var oid_iproute_protocol = '.1.3.6.1.2.1.4.21.1.9'
+iproute_protocol.getSubtree({ oid: oid_iproute_protocol }, function (err, varbinds) {
+  varbinds.forEach(function (data) {
+    iprouteprotocol.push({protocol: data.value})
+    // console.log(data.value)
+  })
+  iproute_protocol.close()
 })
 // ///////////////////////////////////////////////////////////////
 app.get('/name', function (req, res) {
   res.send(vb)
-})
-app.get('/ip', function (req, res) {
-  res.send(ip)
 })
 app.get('/subnet', function (req, res) {
   res.send(subnet)
 })
 app.get('/iproute', function (req, res) {
   res.send(iproute)
+})
+app.get('/speed', function (req, res) {
+  res.send(speed)
+})
+app.get('/vlan', function (req, res) {
+  res.send(vlan)
+})
+app.get('/iproutetype', function (req, res) {
+  res.send(iproutetype)
+})
+app.get('/iprouteprotocol', function (req, res) {
+  res.send(iprouteprotocol)
 })
 app.use(express.static('public'))
 app.listen(7001, function () {
--- public/todo.js
+++ public/todo.js
@@ -6,6 +6,8 @@ angular.module('todoApp', [])
     getName()
     getIp()
     getSubnet()
+    getIproutetype()
+    getIprouteprotocol()
     function getName () {
       $http.get('/name').then(function success (response) {
         app.name = response.data
@@ -19,6 +21,16 @@ angular.module('todoApp', [])
     function getSubnet () {
       $http.get('/subnet').then(function success (response) {
         app.subnet = response.data
+      })
+    }
+    function getIproutetype () {
+      $http.get('/iproutetype').then(function success (response) {
+        app.iproutetype = response.data
+      })
+    }
+    function getIprouteprotocol () {
+      $http.get('/iprouteprotocol').then(function success (response) {
+        app.iprouteprotocol = response.data
       })
     }
   })
